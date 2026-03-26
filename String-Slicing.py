6d4edccf64b89d8d0ac9ced2d2165c6db5eb9c5a
RachaelLynn93 <rachaellucas65@yahoo.com>
2026-03-26 01:39:46 -0500

    Added String-Slicing.py to this folder and an example of stringslicing example

---
+++ String-Slicing.py
@@ -0,0 +1,7 @@
+def reverse_string(text):
+    # Returns the string stepping backwards by 1
+    return text[::-1]
+
+# Testing the function
+print(reverse_string("hello")) 
+print(reverse_string("Python"))
